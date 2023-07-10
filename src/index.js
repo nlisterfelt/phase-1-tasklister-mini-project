ebe3c18d6478a7866546ebef4ce881e18fa3e2e6
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-07-10 17:51:08 -0500

    add and delete todos

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    buildToDo(e.target.new_task_description.value)
+    console.log(e.target.new_task_description.value)
+    form.reset()
+  })
 });
+
+function buildToDo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${todo} `
+  document.querySelector('#tasks').appendChild(p)
+  p.appendChild(btn)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
